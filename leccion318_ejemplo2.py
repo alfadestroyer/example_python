3a8df2a70f66ef7e0236f22042dc4a9d56929eac
Manuel Matos <manuelematosp@outlook.com>
2023-05-06 20:58:57 -0400

    mi primer ejercicio en python

---
+++ leccion318_ejemplo2.py
@@ -0,0 +1,13 @@
+# Se leen dos números
+numero1 = int(input("Ingresa el primer número: "))
+numero2 = int(input("Ingresa el segundo número: "))
+
+# Elige el número más grande
+if numero1 > numero2:
+    mayor_numero = numero1
+else:
+    mayor_numero = numero2
+
+# Imprime el resultado
+print("El número mayor es:", mayor_numero)
+
