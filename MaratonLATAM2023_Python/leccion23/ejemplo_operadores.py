dee361b62ebaf4090dedde5d53acc2a9a90deb72
Manuel Matos <manuelematosp@outlook.com>
2023-05-09 16:32:40 -0400

    Se subio ejemplos de Operadores con division

--- MaratonLATAM2023_Python/leccion23/ejemplo_operadores.py
+++ MaratonLATAM2023_Python/leccion23/ejemplo_operadores.py
@@ -1,16 +1,26 @@
 # Operadores _ Herramientas de manipulacion de datos
 
-# Operador de suma(+)
+# Operador de suma(+).
+print("suma.")
 print(2+2)
 
-# Operador con Exponenciación
+# Operador de Exponenciación(**).
+print("Exponenciacion.")
 print(2 ** 3)
 print(2 ** 3.)
 print(2. ** 3)
 print(2. ** 3.)
 
-# Operador con Multiplicacion
+# Operador de Multiplicacion(*).
+print("Multiplicacion.")
 print(2 * 3)
 print(2 * 3.)
 print(2. * 3)
 print(2. * 3.)
+
+# OPerador de Division(/).
+print("Division.)")
+print(6 / 3)
+print(6 / 3.)
+print(6. / 3)
+print(6. / 3.)
