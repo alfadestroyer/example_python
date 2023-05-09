5fdfccc4f614e91d5580590253b2614dfb5c67e5
Manuel Matos <manuelematosp@outlook.com>
2023-05-09 17:37:27 -0400

    Se subio ejemplos de Variables

---
+++ MaratonLATAM2023_Python/leccion24/ejemplo_variables.py
@@ -0,0 +1,11 @@
+# Variables.
+
+print("ejemplo 1.")
+var = 1
+print(var)
+
+print("ejemplo 2.")
+var = 1
+balance_cuenta = 10000.0
+nombre_cliente = 'Jose Marquez'
+print(var, nombre_cliente , balance_cuenta)
