498d5319df248e5509670ecab78171982337c380
Manuel Matos <manuelematosp@outlook.com>
2023-05-10 22:20:17 -0400

    Se subio ejemplos de Entradas, Funsion input()

--- MaratonLATAM2023_Python/leccion26/ejemplo_entradas.py
+++ MaratonLATAM2023_Python/leccion26/ejemplo_entradas.py
@@ -1,4 +1,9 @@
+# Ejemplo 1.
 print("Dime lo que sea...")
 dime_algo = input()
 print("Hmm...", dime_algo, "... ¿en serio?")
 
+# Ejemplo 2.
+ingreso_numero = float(input("Ingresa un número: "))
+salida_numero = ingreso_numero ** 2.0
+print(ingreso_numero, "al cuadrado es", salida_numero)
