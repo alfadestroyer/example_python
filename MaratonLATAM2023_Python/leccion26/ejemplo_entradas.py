1973e29bfdb9858786b2c584f393e91a6c8bbafe
Manuel Matos <manuelematosp@outlook.com>
2023-05-12 12:46:10 -0400

    Se subio ejemplos de Entradas, Funsion input() con conversión de tipo

--- MaratonLATAM2023_Python/leccion26/ejemplo_entradas.py
+++ MaratonLATAM2023_Python/leccion26/ejemplo_entradas.py
@@ -11,8 +11,13 @@ salida_numero = ingreso_numero ** 2.0
 print(ingreso_numero, "al cuadrado es", salida_numero)
 
 # Ejemplo 3.
-print("Ejemplo 3")
+print("Ejemplo 3.1")
 leg_a = float(input("Ingresa la longitud del primer cateto: "))
 leg_b = float(input("Ingresa la longitud del segundo cateto: "))
 hypo = (leg_a**2 + leg_b**2) ** .5
 print("La longitud de la hipotenusa es:", hypo)
+
+print("Ejemplo 3.2")
+leg_a = float(input("Ingresa la longitud del primer cateto: "))
+leg_b = float(input("Ingresa la longitud del segundo cateto: "))
+print("La longitud de la hipotenusa es:", (leg_a**2 + leg_b**2) ** .5)
