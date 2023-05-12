851a87bacdf42d4f09e015b263609a0c49316544
Manuel Matos <manuelematosp@outlook.com>
2023-05-12 18:53:54 -0400

    Se subio ejemplos de sentencias condicionales

---
+++ MaratonLATAM2023_Python/leccion31/ejemplo_Toma_decisiones.py
@@ -0,0 +1,42 @@
+# Operadores de comparación 
+#Toma de decisiones en Python
+
+# El operador igual a (==).
+print("Ejemplo con el operador igual a (==)")
+print("compara dos valores en una variable")
+var = 0  # Asignando 0 a var
+print(var == 0)
+var = 1  # Asignando 1 a var
+print(var == 0)
+
+# El operador no es igual a (!=).
+print("Ejemplo con el operador no es igual a (!=)")
+print("compara dos valores en una variable")
+var = 0  # Asignando 0 a var
+print(var != 0) 
+var = 1  # Asignando 1 a var
+print(var != 0)
+
+# Operador  mayor que (>).
+print("Ejemplo con el operador  mayor que (>)")
+print("compara dos valores en una variable")
+var = 0  # Asignando 0 a var
+print(var > 0) 
+var = 1  # Asignando 1 a var
+print(var > 0)
+
+# Operador  menor que (<).
+print("Ejemplo con el operador  menor que (<)")
+print("compara dos valores en una variable")
+var = 0  # Asignando 0 a var
+print(var < 0) 
+var = 1  # Asignando 1 a var
+print(var < 0)
+
+# Operador mayor o igual que (>=).
+print("Ejemplo con el operador mayor o igual que (>=).")
+print("compara dos valores en una variable")
+var = 0  # Asignando 0 a var
+print(var >= 0) 
+var = 10  # Asignando 10 a var
+print(var >= 20)
