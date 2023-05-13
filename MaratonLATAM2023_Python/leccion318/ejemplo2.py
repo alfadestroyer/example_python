deb0c6d5ea44f9cdb1fb98b6ad1ce63c7f43ceee
Manuel Matos <manuelematosp@outlook.com>
2023-05-13 18:06:31 -0400

    Se subio ejemplos de sentencias condicionales if_ else

--- MaratonLATAM2023_Python/leccion318/ejemplo2.py
+++ MaratonLATAM2023_Python/leccion318/ejemplo2.py
@@ -1,4 +1,5 @@
 # Se leen dos números
+print("Ejemplo con el uso de condicionadores if_else")
 
 numero1 = int(input("Ingresa el primer número: "))
 numero2 = int(input("Ingresa el segundo número: "))
@@ -11,5 +12,17 @@ else:
     mayor_numero = numero2
 
 # Imprime el resultado
+
+print("El número mayor es: ", mayor_numero)
+
+# Otra forma aceptada por Python.
+
+print("Ejemplo con el uso de condicionadores if_else")
+
+numero1 = int(input("Ingresa el primer número: "))
+numero2 = int(input("Ingresa el segundo número: "))
+
+if numero1 > numero2: mayor_numero = numero1
+else: mayor_numero = numero2
 
 print("El número mayor es: ", mayor_numero)
